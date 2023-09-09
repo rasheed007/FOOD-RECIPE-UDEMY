188ff5792b017fad614cc0f95cd5d9e4620b080b
Rasheed Olayanju <dimexylora@gmail.com>
2023-09-10 00:46:33 +0100

    created the rendered html file for course

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,22 @@
+var courses = [
+    {
+        title: "Asun peppered Cakes",
+        cost: 500
+    },
+    {
+        title: "Goatmeat roasted vegetables",
+        cost: 750
+    },
+    {
+        title: "Jambalaya spicy Rice",
+        cost: 400
+    }
+];
 exports.showCourses = (req, res) => {
-    res.render("courses");
+    const body = courses;
+    res.render("courses", {
+        offeredCourses: courses, body
+    });
 };
 
 exports.showSignUp = (req, res) => {
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const homeController = require('./controllers/homeController')
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
@@ -17,6 +18,10 @@ app.get("/", (req, res) => {
     const body = 'Welcome to the home page of Recipe Cuisine!';    
     res.render("home", { title, body }); 
 });
+
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact", homeController.postedSignUpForm);
 
 
 app.listen(app.get("port"), () => {
